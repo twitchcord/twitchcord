02feabc42e2ff4aa594dc7538896619303037036
MoreThanTom <tomsputz@outlook.com>
2019-03-14 05:44:47 +0000

    Fix: Race condition for removing CSP headers

--- src/modules/OldLoader/index.ts
+++ src/modules/OldLoader/index.ts
@@ -4,12 +4,14 @@ import { join } from "path";
 app.on("renderer-preload-paths", (evt: Event) =>
   evt.returnValue.push(join(__dirname, "renderer"))
 );
-session.defaultSession.webRequest.onHeadersReceived((details, done) => {
-  const responseHeaders = details.responseHeaders as { [key: string]: string }
-
-  for (const header in responseHeaders)
-    if (header.toLowerCase().startsWith('content-security-policy'))
-      delete responseHeaders[header]
-
-  done({ cancel: false, responseHeaders})
+app.whenReady().then(() => {
+  session.defaultSession.webRequest.onHeadersReceived((details, done) => {
+    const responseHeaders = details.responseHeaders as { [key: string]: string }
+  
+    for (const header in responseHeaders)
+      if (header.toLowerCase().startsWith('content-security-policy'))
+        delete responseHeaders[header]
+  
+    done({ cancel: false, responseHeaders})
+  })
 })
