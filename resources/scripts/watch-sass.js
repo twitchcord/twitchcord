b410207892de2f3625bb8ec67f4f798554de287f
MoreThanTom <tomsputz@outlook.com>
2019-03-22 22:04:21 +0000

    Compile multiple stylesheets

--- resources/scripts/watch-sass.js
+++ resources/scripts/watch-sass.js
@@ -1,36 +1,40 @@
 const { render } = require('node-sass')
-const { join, dirname } = require('path')
-const { watch, writeFile } = require('fs')
+const { join, dirname, basename } = require('path')
+const { watch, writeFile, readdirSync } = require('fs')
 const btoa = require("btoa")
 
-const IN = join(__dirname, '../styles/main.scss')
-const OUT = join(__dirname, '../../assets/styles/main.css')
+const IN = join(__dirname, '../styles/')
+const OUT = join(__dirname, '../../assets/styles/')
+const FILES = readdirSync(IN).filter(file => file.endsWith('.scss'))
 
-function CompileSass() {
+function CompileAll() {
   console.log("Compiling...")
-  render({
-    file: IN,
-    outFile: IN,
-    sourceMap: true,
-    outputStyle: "compressed"
-  }, (err, result) => {
-    if (err) return console.error(err);
-    const SourceMap = JSON.parse(result.map.toString());
-    SourceMap.sources = SourceMap.sources.map(source => "file:///" + join(dirname(IN), source));
-    console.log(JSON.stringify(SourceMap.sources))
-    writeFile(
-      OUT,
-      Buffer.concat([result.css, Buffer.from("\n/*# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(SourceMap)) +" */`")]),
-      err => { if (err) console.error(err); }
-    );
-  })
+  for (const file of FILES) {
+    const path = join(IN, file)
+    render({
+      file: path,
+      outFile: path,
+      sourceMap: true,
+      outputStyle: "compressed"
+    }, (err, result) => {
+      if (err) return console.error(err);
+      const SourceMap = JSON.parse(result.map.toString());
+      SourceMap.sources = SourceMap.sources.map(source => "file:///" + join(IN, source));
+      writeFile(
+        join(OUT, basename(file, '.scss') + '.css'),
+        Buffer.concat([result.css, Buffer.from("\n/*# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(SourceMap)) +" */`")]),
+        err => { if (err) console.error(err); }
+      );
+    })
+  }
 }
+
 let timeout
 watch(dirname(IN), {
   recursive: true
 }, (event, filename) => {
   if (!filename.endsWith('scss')) return
-  timeout = timeout || setTimeout(() => (timeout = 0, CompileSass()), 100)
+  timeout = timeout || setTimeout(() => (timeout = 0, CompileAll()), 100)
 })
 
-CompileSass()
+CompileAll()
