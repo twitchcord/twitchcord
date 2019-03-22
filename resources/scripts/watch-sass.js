a35fda125f24aaca0518973687e350347a910ec1
MoreThanTom <tomsputz@outlook.com>
2019-03-22 21:48:42 +0000

    Fix: Properly link sourcemaps

--- resources/scripts/watch-sass.js
+++ resources/scripts/watch-sass.js
@@ -1,6 +1,7 @@
 const { render } = require('node-sass')
 const { join, dirname } = require('path')
 const { watch, writeFile } = require('fs')
+const btoa = require("btoa")
 
 const IN = join(__dirname, '../styles/main.scss')
 const OUT = join(__dirname, '../../assets/styles/main.css')
@@ -9,12 +10,19 @@ function CompileSass() {
   console.log("Compiling...")
   render({
     file: IN,
-    outFile: OUT,
-    sourceMapEmbed: true,
+    outFile: IN,
+    sourceMap: true,
     outputStyle: "compressed"
   }, (err, result) => {
-    if (err) return console.error(err)
-    writeFile(OUT, result.css.toString(), function() {})
+    if (err) return console.error(err);
+    const SourceMap = JSON.parse(result.map.toString());
+    SourceMap.sources = SourceMap.sources.map(source => "file:///" + join(dirname(IN), source));
+    console.log(JSON.stringify(SourceMap.sources))
+    writeFile(
+      OUT,
+      Buffer.concat([result.css, Buffer.from("\n/*# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(SourceMap)) +" */`")]),
+      err => { if (err) console.error(err); }
+    );
   })
 }
 let timeout
