dc3d9640282528828df7de325069edfc35896c2a
MoreThanTom <tomsputz@outlook.com>
2019-04-03 11:59:22 +0100

    MASSIVE upgrade to lazy loading logic
    - The old code feels kinda ridiculous now

--- src/renderer/patches/AdaptiveLazyChannels.ts
+++ src/renderer/patches/AdaptiveLazyChannels.ts
@@ -1,70 +1,43 @@
-let CHANNEL_HEIGHT = 34;
+// TODO:
+// Proper dependencies - const { Channels, ChannelItem } = require("discord-components")
+// This also needs to access the modules before other dependencies - Module.load("discord-components", -1) might work
 
-const WaitForTruthy = <R>(
-  cb: (...args: any[]) => R,
-  delay: number
-): Promise<R> =>
-  new Promise(res =>
-    setTimeout(() => res(cb() || WaitForTruthy(cb, delay)), delay)
-  );
+const NATIVE_CHANNEL_HEIGHT = 34
+let CHANNEL_HEIGHT = NATIVE_CHANNEL_HEIGHT;
 
-WaitForTruthy(() => {
-  const tc = (global as any)["tc"];
-  return tc && tc.webpack ? tc : false;
-}, 200).then(tc =>
-  WaitForTruthy(() => tc.webpack.findByDisplayName("Channels"), 200).then(
-    Channels => {
-      const Constants = tc.webpack.findByUniqueProperties(["ChannelTypes"]);
-      const ChannelUtil = tc.webpack.findByUniqueProperties([
-        "isChannelCollapsed"
-      ]);
-      const Storage = tc.webpack.findByUniqueProperties(["ObjectStorage", "impl"]).impl
-      CHANNEL_HEIGHT = Storage.get("FOUND_CHANNEL_HEIGHT") || CHANNEL_HEIGHT
-      const _render = Channels.prototype.render;
-      let _getRowHeight: Function;
-      function render(...args: any[]) {
-        this.getRowHeight = _getRowHeight.bind(this);
-        return _render.call(this, ...args);
-      }
-      Channels.prototype.render = function PhaseOne(...args: any[]) {
-        _getRowHeight = function(t: number, index: number = 0) {
-          const e = this;
-          let props = e.props;
-          let channels = props.channels;
-          let collapsedChannels = props.collapsedChannels;
-          let voiceStates = props.voiceStates;
-          let categories = props.categories;
-          let _channel = channels[Constants.ChannelTypes.GUILD_CATEGORY][t];
-          let height = CHANNEL_HEIGHT
-          let f = _channel
-            ? categories[_channel.channel.id][index]
-            : categories.null[index];
-          if (null === f) return 0;
-          var channel = f.channel;
-          if (ChannelUtil.isChannelCollapsed(channel)) return 0;
-          switch (channel.type) {
-            case Constants.ChannelTypes.GUILD_VOICE:
-              var p = voiceStates[channel.id];
-              if (null != p && p.length > 0) {
-                var h = 28 * p.length;
-                channel && collapsedChannels[channel.id] && (h = Math.ceil(h / 6)),
-                  (height += h + 8);
-              }
-          }
-          return height;
-        };
-        Channels.prototype.render = render;
-        const Rendered = this.render(...args);
-        WaitForTruthy(
-          () => Rendered.props.children[1]._owner.return.stateNode,
-          200
-        )
-          .then(el =>
-            WaitForTruthy(() => el.querySelector("[class^=wrapper]"), 200)
-          )
-          .then(el => (console.log("Doin it"), Storage.set("FOUND_CHANNEL_HEIGHT", CHANNEL_HEIGHT = parseInt(getComputedStyle(el).height))));
-        return Rendered;
-      };
+const wait = (ms: number) => new Promise(res => setTimeout(res, ms));
+export async function waitForTruthy<R>(cb: (...args: any[]) => R, delay: number = 200): Promise<R> {
+  try {
+    const val = cb()
+    if (val) return val
+  } catch {}
+  return await wait(delay).then(() => waitForTruthy(cb, delay))
+}
+
+waitForTruthy(() => (window as any).tc.webpack.findByDisplayName("Channels")).then(Channels => {
+  Object.defineProperty(Channels.prototype, "getRowHeight", {
+    set: function(value) {
+      Object.defineProperty(this, "getRowHeight", {
+        configurable: true,
+        enumerable: true,
+        value: function(...args: any[]) {
+          const ret = value(...args)
+          return ret === 0 ? 0 : ret === NATIVE_CHANNEL_HEIGHT ? CHANNEL_HEIGHT : ret - NATIVE_CHANNEL_HEIGHT + CHANNEL_HEIGHT
+        }
+      })
     }
-  )
-);
+  })
+});
+waitForTruthy(() => (window as any).tc.webpack.findByDisplayName("ChannelItem")).then(ChannelItem => {
+  const _render = ChannelItem.prototype.render
+  ChannelItem.prototype.render = function(...args: any[]) {
+    ChannelItem.prototype.render = _render
+    const rendered = this.render(...args)
+    rendered.ref = function(stateNode: { ref: HTMLElement }) {
+      if (!stateNode) return
+      CHANNEL_HEIGHT = parseInt(getComputedStyle(stateNode.ref).height)
+    }
+    return rendered
+  }
+})
+
