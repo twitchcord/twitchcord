e204ecf91b54f087aca2110cb21eabd1b9144a3a
MoreThanTom <tomsputz@outlook.com>
2019-04-12 08:59:33 +0100

    Enable experimental chromium features
    - At request of Baked for `backdrop-filter`

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ import { join, dirname } from "path";
       options.webPreferences = Object.assign({}, options.webPreferences)
       options.webPreferences.preload = join(dirname(DISCORD_APP_ROOT), "app");
       options.webPreferences.webSecurity = false;
-      const win = new BrowserWindow(options)
+      options.webPreferences.experimentalFeatures = true;
       // Allow the renderer process to act on the options
       win.__options = userOptions
       return win;
