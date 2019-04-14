fd7306095909866c8fac9f4aa06d6f096b19e145
BakedPVP <gameking36@msn.com>
2019-04-14 11:37:06 -0400

    - Let's try this again

--- src/renderer/old-core/api2.js
+++ src/renderer/old-core/api2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const components = require('./modules/components.js');
 const webpack = require('./modules/webpack.js');
 const react = require('./modules/react.js');
@@ -16,22 +15,3 @@ module.exports = tc = {
   classes: classes
     // nav: new Nav()
 };
-=======
-const classes = require('./modules/classes.js');
-// const components = require('./modules/components.js');
-const webpack = require('./modules/webpack.js');
-// const react = require('./modules/react.js');
-// const settings = require('./modules/settings.js');
-const utilities = require('./modules/utilities.js');
-
-
-module.exports = tc = {
-  classes: classes,
-  // components: components,
-  webpack: webpack,
-  // react: react,
-  // settings: settings,
-  utils: utilities
-    // nav: new Nav()
-};
->>>>>>> 9f486902c4d3a5b198c1402007d82bc6a1916d73
